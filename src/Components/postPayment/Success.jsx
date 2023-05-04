d87127dfe938200ca6cf871fa1f77f4d2e000411
anil_agnito <anil@agnitotechnologies.com>
2023-05-04 18:28:37 +0530

    payment changes to status

--- src/Components/postPayment/Success.jsx
+++ src/Components/postPayment/Success.jsx
@@ -1,5 +1,5 @@
 import { Stack } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useLocation } from "react-router-dom";
 import { PaymentValidate } from "../../utils";
 
@@ -10,39 +10,36 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Loader from "../Loader";
 
 const Success = () => {
   const location = useLocation();
   const payment_information = location.search.split("?")[1].split("&");
   const paymentId = payment_information[0].split("=")[1];
-  const PayerID = payment_information[1].split("=")[1];
+  const PayerID = payment_information[2].split("=")[1];
   console.log(payment_information);
+  const [res, setRes] = useState({});
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const validatePayment = async () => {
       if (paymentId && PayerID) {
-        console.log(paymentId, PayerID);
         const body = {
           payer_id: PayerID,
           payment_id: paymentId,
         };
-
+        setLoading(true)
         const res = await PaymentValidate(body);
-        if (res.response.data.message === "Payment not validated") {
-          alert("Payment Failed");
-        }
-        if (res.response.data.message === "Payment validated!") {
-          alert("Payment validated Successfully!");
-        }
-        if (res.response.data.message === "application is already paid") {
-          alert("Application is already paid");
-        }
-        console.log(res);
+        console.log(res.res,"111111111111111");
+        setRes(res.response)
+        setLoading(false)
       }
     };
     validatePayment();
   }, []);
 
   return (
+    <>
+    {loading && <Loader />}
     <Stack
       sx={{
         width: "100%",
@@ -54,6 +51,7 @@ const Success = () => {
         // bgcolor: "primary.main",
       }}
     >
+      {res.status === 200 ?
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography variant="h4">Payment successfull</Typography>
@@ -66,10 +64,29 @@ const Success = () => {
           />
         </CardContent>
         <CardActions className="d-flex justify-content-center">
-          <Button size="small">Go Back To Home Page</Button>
         </CardActions>
       </Card>
+      :
+      
+      (<Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h4">Payment Unsuccessfull</Typography>
+        </CardContent>
+        <CardContent className="d-flex justify-content-center">
+          <img
+            src="https://i.pinimg.com/originals/90/13/f7/9013f7b5eb6db0f41f4fd51d989491e7.gif"
+            height={"100px"}
+            width={"200px"}
+          />
+        </CardContent>
+        <CardActions className="d-flex justify-content-center">
+        </CardActions>
+      </Card>)
+      }
+
     </Stack>
+  
+  </>
   );
 };
 
